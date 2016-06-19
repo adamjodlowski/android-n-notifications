c42406c2a62095596f0e417c13608bd8a820b433
Adam Jodlowski <adamjodlowski@users.noreply.github.com>
2016-06-19 17:24:24 +0200

    Add empty project with button

---
+++ app/src/main/java/io/github/adamjodlowski/notifications/MainActivity.java
@@ -0,0 +1,26 @@
+package io.github.adamjodlowski.notifications;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(buttonClickListener);
+    }
+
+    private View.OnClickListener buttonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            // TODO display notification
+        }
+    };
+}
