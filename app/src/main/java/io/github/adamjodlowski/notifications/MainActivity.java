407bc2d3e16d4dd5d3de1f167a660cca54b44a28
Adam Jodlowski <adamjodlowski@users.noreply.github.com>
2016-06-19 17:37:21 +0200

    Show basic notification

--- app/src/main/java/io/github/adamjodlowski/notifications/MainActivity.java
+++ app/src/main/java/io/github/adamjodlowski/notifications/MainActivity.java
@@ -1,6 +1,8 @@
 package io.github.adamjodlowski.notifications;
 
+import android.app.NotificationManager;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -8,6 +10,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
+    private int notificationId = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +23,18 @@ public class MainActivity extends AppCompatActivity {
     private View.OnClickListener buttonClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            // TODO display notification
+
+            // NotificationCompat Builder takes care of backwards compatibility and
+            // provides clean API to create rich notifications
+            NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(MainActivity.this)
+                    .setSmallIcon(android.R.drawable.ic_dialog_info)
+                    .setContentTitle("Notification title")
+                    .setContentText("Content text");
+
+            // Obtain NotificationManager system service in order to show the notification
+            NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            notificationManager.notify(notificationId, mBuilder.build());
+
         }
     };
 }
